f853844051101abed9a96eb758c338c8d248f713
Félix Girault <felix.girault@gmail.com>
2025-03-10 09:18:12 +0100

    Fixed loading of references
    They are now loaded only if they are not already, which prevents
    resetting state that doesn't need to be.

--- src/panel/listeners/reference.ts
+++ src/panel/listeners/reference.ts
@@ -2,7 +2,7 @@ import type {AppStartListening} from '../middlewares/listener';
 import {resetResults} from '../slices/audit';
 import {setHelpers} from '../slices/helpers';
 import {setInstructions} from '../slices/instructions';
-import {setReferenceData, setVersion} from '../slices/reference';
+import {selectVersion, setReferenceData, setVersion} from '../slices/reference';
 import {fetchHelpers} from '../utils/helpers';
 import {fetchInstructions} from '../utils/instructions';
 import {fetchReference, flattenReference} from '../utils/reference';
@@ -11,16 +11,20 @@ export const addReferenceListeners = (startListening: AppStartListening) => {
 	startListening({
 		actionCreator: setVersion,
 		async effect({payload: version}, api) {
-			const [reference, helpers, instructions] = await Promise.all([
-				fetchReference(version),
+			const reference = await fetchReference(version);
+			const currentVersion = selectVersion(api.getState());
+
+			if (reference.version === currentVersion) {
+				return;
+			}
+
+			const [helpers, instructions] = await Promise.all([
 				fetchHelpers(version),
 				fetchInstructions(version)
 			]);
 
-			const flattened = flattenReference(reference);
-
 			api.dispatch(resetResults());
-			api.dispatch(setReferenceData(flattened));
+			api.dispatch(setReferenceData(flattenReference(reference)));
 			api.dispatch(setHelpers(helpers));
 			api.dispatch(setInstructions(instructions));
 		}
