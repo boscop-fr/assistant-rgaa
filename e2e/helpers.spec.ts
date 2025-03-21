4463e7c7e201eaff641ebc853e78788add96a41f
Félix Girault <felix.girault@gmail.com>
2025-03-21 10:43:50 +0100

    Added end-to-end tests of the panel

--- e2e/helpers.spec.ts
+++ e2e/helpers.spec.ts
@@ -15,21 +15,27 @@ test('should toggle styles', async ({helpersPage: page}) => {
 	await expect(page.elementHiddenViaStyleTag).toBeHidden();
 	await expect(page.elementHiddenViaStyleAttribute).toBeHidden();
 
-	await page.sendMessage(applyHelpers([{helper: 'disableAllStyles'}]));
+	await page.sendMessage(
+		'runtime',
+		applyHelpers([{helper: 'disableAllStyles'}])
+	);
 	await expect(page.elementHiddenViaStyleSheet).toBeVisible();
 	await expect(page.elementHiddenViaStyleTag).toBeVisible();
 	await expect(page.elementHiddenViaStyleAttribute).toBeVisible();
 
-	await page.sendMessage(revertActiveHelpers());
+	await page.sendMessage('runtime', revertActiveHelpers());
 	await expect(page.elementHiddenViaStyleSheet).toBeHidden();
 	await expect(page.elementHiddenViaStyleTag).toBeHidden();
 	await expect(page.elementHiddenViaStyleAttribute).toBeHidden();
 });
 
 test('should extract headings hierarchy', async ({helpersPage: page}) => {
-	await page.sendMessage(applyHelpers([{helper: 'headingsHierarchy'}]));
+	await page.sendMessage(
+		'runtime',
+		applyHelpers([{helper: 'headingsHierarchy'}])
+	);
 
-	await expect(await page.lastSentMessage()).toEqual({
+	await page.expectNextSentMessage('runtime', {
 		type: 'helpers/headingsHierarchy/set',
 		payload: [
 			{level: 1, text: 'Heading 1'},
@@ -38,10 +44,13 @@ test('should extract headings hierarchy', async ({helpersPage: page}) => {
 		]
 	});
 
-	await page.sendMessage(revertActiveHelpers());
-	await page.sendMessage(applyHelpers([{helper: 'headingsHierarchy'}]));
+	await page.sendMessage('runtime', revertActiveHelpers());
+	await page.sendMessage(
+		'runtime',
+		applyHelpers([{helper: 'headingsHierarchy'}])
+	);
 
-	await expect(await page.lastSentMessage()).toEqual({
+	await page.expectNextSentMessage('runtime', {
 		type: 'helpers/headingsHierarchy/set',
 		payload: [
 			{level: 1, text: 'Heading 1'},
@@ -51,9 +60,8 @@ test('should extract headings hierarchy', async ({helpersPage: page}) => {
 	});
 
 	await page.removeIntermediateHeading();
-	await page.waitForNextSentMessage();
 
-	await expect(await page.lastSentMessage()).toEqual({
+	await page.expectNextSentMessage('runtime', {
 		type: 'helpers/headingsHierarchy/set',
 		payload: [
 			{level: 1, text: 'Heading 1'},
