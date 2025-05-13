36fa8f25817f37fa2f32cdc016efca0371077480
Félix Girault <felix.girault@gmail.com>
2025-05-13 14:03:56 +0200

    fixup! WIP: Added selector catalogs

--- build/helpers-loader.ts
+++ build/helpers-loader.ts
@@ -1,32 +1,58 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import type {LoaderContext} from '@rspack/core';
 
-export type SelectorCatalog = Record<string, string[]>;
-export type CompiledSelectorCatalog = Record<string, string>;
+type SelectorCatalog = Record<string, string[]>;
+type HelpersLoaderOptions = {
+	catalogsPath: string;
+	pseudoClass: string;
+};
 
-const compileSelectorCatalog = (
-	catalog: SelectorCatalog
-): CompiledSelectorCatalog =>
-	Object.fromEntries(
-		Object.entries(catalog).map(([name, selectors]) => [
+const escapeJsonString = (string: string) =>
+	JSON.stringify(string).substring(1, string.length - 1);
+
+export default function helpersLoader(
+	this: LoaderContext<HelpersLoaderOptions>,
+	content: string
+) {
+	const {catalogsPath = `${this.context}/catalogs`, pseudoClass = ':rgaa'} =
+		this.getOptions();
+
+	if (!content.includes(pseudoClass)) {
+		return content;
+	}
+
+	const catalogPath = `${catalogsPath}/${path.basename(this.resourcePath)}`;
+
+	if (!fs.existsSync(catalogPath)) {
+		throw new Error(`No selector catalog found at \`${catalogPath}\``);
+	}
+
+	const rawCatalog = JSON.parse(
+		fs.readFileSync(catalogPath, 'utf-8')
+	) as SelectorCatalog;
+
+	const catalog = Object.fromEntries(
+		Object.entries(rawCatalog).map(([name, selectors]) => [
 			name,
 			`:is(${selectors.join(',')})`
 		])
 	);
 
-export default function (content) {
-	const catalogPath = `${this.context}/catalogs/${path.basename(this.resourcePath)}`;
+	const pattern = `${pseudoClass}\\(([^)]+)\\)`;
+	const rx = new RegExp(pattern, 'gi');
 
-	if (!fs.existsSync(catalogPath)) {
-		return content;
-	}
+	return content.replaceAll(rx, (selector, key) => {
+		if (!(key in catalog)) {
+			const relativePath = path.relative(this.rootContext, this.resourcePath);
+			const relativeCatalogPath = path.relative(this.rootContext, catalogPath);
 
-	const rawCatalog = JSON.parse(fs.readFileSync(catalogPath, 'utf-8'));
-	const catalog = compileSelectorCatalog(rawCatalog);
-	const keys = Object.keys(catalog);
-	const keysMatcher = `(${keys.join('|')})`;
-	const pseudoMatcher = new RegExp(`:rgaa\\(${keysMatcher}\\)`, 'gi');
+			throw new Error(
+				`No \`${key}\` selector found in \`${relativeCatalogPath}\`` +
+					`(as required by \`${selector}\` in ${relativePath})`
+			);
+		}
 
-	// @todo Safer replacement to avoid JSON formatting issues
-	return content.replaceAll(pseudoMatcher, (_, key) => catalog[key]);
+		return escapeJsonString(catalog[key]);
+	});
 }
