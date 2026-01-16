a157482f100d92604bc11997c4151528082b6de5
Félix Girault <felix.girault@gmail.com>
2026-01-16 14:04:51 +0100

    WIP

--- src/panel/utils/helpers.ts
+++ src/panel/utils/helpers.ts
@@ -1,10 +1,43 @@
-import type {HelpersByTest} from '../../common/types';
+import type {CompiledSelectorCatalog, HelpersByTest, SelectorCatalog} from '../../common/types';
 
-/*
- * retrieve the helpers mapping full json object from a given reference version
- */
-export const fetchHelpers = (version: string) =>
+const compileSelectorCatalog = (catalog: SelectorCatalog) : CompiledSelectorCatalog=>
+	Object.fromEntries(
+		Object.entries(catalog).map(([name, selectors]) => [
+			name,
+			`:is(${selectors.join(',')})`
+		])
+	)
+
+const expandSelectorCatalog = (selector: string, catalog: CompiledSelectorCatalog) => {
+	
+};
+
+const fetchCatalog = (version: string) =>
+	import(
+		/* webpackChunkName: "helpers-" */
+		`../../../data/helpers/catalogs/${version}.json`
+	).then((module) => module.default as SelectorCatalog);
+
+const fetchRawHelpers = (version: string) =>
 	import(
 		/* webpackChunkName: "helpers-" */
 		`../../../data/helpers/${version}.json`
 	).then((module) => module.default as HelpersByTest);
+
+/*
+ * retrieve the helpers mapping full json object from a given reference version
+ */
+export const fetchHelpers = async (version: string) => {
+	const [rawHelpers, catalog] = await Promise.all([
+		fetchRawHelpers(version),
+		fetchCatalog(version)
+	]);
+
+	for (const test in rawHelpers) {
+		const helpers = rawHelpers[test as keyof typeof rawHelpers];
+
+		for (let i = 0; i < helpers.length; i++) {
+			helpers[i];
+		}
+	}
+};
